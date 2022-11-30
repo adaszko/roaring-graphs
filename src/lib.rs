37a55d9c347ddbfa937ec7840addfb40f22f3c6c
Adam Szkoda <adaszko@gmail.com>
2022-11-30 13:24:01 +0100

    Useful re-exports

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,6 @@ trait TraversableDirectedGraph {
     fn extend_with_children(&self, children: &mut Vec<u32>, u: u32);
     fn extend_with_parents(&self, parents: &mut Vec<u32>, v: u32);
 }
+
+pub use dag::{arb_dag, DirectedAcyclicGraph};
+pub use digraph::{arb_tree, DirectedGraph};
